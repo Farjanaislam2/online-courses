40a82a4b9fa334440d701b72e9ef662ae309a250
Farjanaislam2 <farjananika2@gmail.com>
2023-03-18 23:00:24 -0700

    n

---
+++ src/component/damage/damage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const damage = () => {
+    return (
+        <div>
+            <h1>sdafsdgs</h1>
+            <h2>dfsdfsdg</h2>
+        </div>
+    );
+};
+
+export default damage;
